320ca4d420276aeff1fd74efec735bb9e5b8e9e9
ferares <fermin@ares.uy>
2023-10-25 10:40:32 -0300

    Cursor animation using css

--- components/Cursor.tsx
+++ components/Cursor.tsx
@@ -1,20 +1,14 @@
-'use client'
-
 import '../styles/cursor.css'
-
-import { useEffect, useState } from 'react'
 
 import { Mode } from '@/types/mode'
 
 function Cursor({ mode }: { mode: Mode }) {
-  const [blink, setBlink] = useState(true)
-  useEffect(() => { setTimeout(() => setBlink(!blink), 700) }, [blink])
 
   if (mode === 'read') return(null)
 
   return(
     <div className="cursor-wrapper">
-      <span className={`cursor ${blink ? 'cursor--off' : ''}`}></span>
+      <span className="cursor"></span>
     </div>
   )
 }
